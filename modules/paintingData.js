341acfa6d519e0dc51461d3ad66115128d4907fb
QiQi-OU-IT <2251052100quan@ou.edu.vn>
2025-06-05 21:39:38 +0700

    Update painting data, artworks, and rebuild dist folder

--- modules/paintingData.js
+++ modules/paintingData.js
@@ -1,23 +1,62 @@
 export const paintingData = [
   // Front Wall
-  ...Array.from({ length: 4 }, (_, i) => ({
-    // Array.from creates an array from an array-like object. The first parameter is the array-like object. The second parameter is a map function that is called for each element in the array-like object. The map function takes two parameters: the element and the index. The map function returns the value that will be added to the new array. In this case, we are returning an object with the painting data. `_` is a placeholder for the element. We don't need it because we are not using the element. `i` is the index. We use it to set the painting number.
-    imgSrc: `artworks/${i + 1}.jpg`, // `i + 1` is the painting number. We add 1 to the index because the index starts at 0 but we want the painting numbers to start at 1.
-    width: 5, // width of the painting
-    height: 3, // height of the painting
-    position: { x: -15 + 10 * i, y: 2, z: -19.5 }, // position of the painting
-    rotationY: 0, // rotation of the painting
-    info: {
-      // info about the painting
-      title: `Van Gogh ${i + 1}`,
-      artist: 'Vincent van Gogh',
-      description: `This is one of the masterpieces by Vincent van Gogh, showcasing his unique style and emotional honesty. Artwork ${
-        i + 1
-      } perfectly encapsulates his love for the beauty of everyday life.`,
-      year: `Year ${i + 1}`,
-      link: 'https://github.com/theringsofsaturn',
-    },
-  })),
+{
+  imgSrc: `artworks/27.jpg`,
+  width: 5,
+  height: 3,
+  position: { x: -15, y: 2, z: -19.5 },
+  rotationY: 0,
+  info: {
+    title: `QiQi x Biển Cam Ranh`,
+    artist: 'LeVietHaiQuan',
+    description: `Biển Cam Ranh đẹp quá`,
+    year: `2025`,
+    link: 'https://www.facebook.com/imagoodsupport',
+  },
+},
+{
+  imgSrc: `artworks/2.jpg`,
+  width: 5,
+  height: 3,
+  position: { x: -5, y: 2, z: -19.5 },
+  rotationY: 0,
+  info: {
+    title: `N.I.M`,
+    artist: 'Như Quỳnh',
+    description: `Như Quỳnh chụp đẹp quá`,
+    year: `2023`,
+    link: 'https://www.facebook.com/imagoodsupport',
+  },
+},
+{
+  imgSrc: `artworks/3.jpg`,
+  width: 5,
+  height: 3,
+  position: { x: 5, y: 2, z: -19.5 },
+  rotationY: 0,
+  info: {
+    title: `Đoàn - Hội CNTT OU`,
+    artist: 'N.I.M PhotoClub',
+    description:'Hành trình về nguồn tại đạo Gạc Ma',
+    year: `2025`,
+    link: 'https://www.facebook.com/imagoodsupport',
+  },
+},
+{
+  imgSrc: `artworks/23.jpg`,
+  width: 5,
+  height: 3,
+  position: { x: 15, y: 2, z: -19.5 },
+  rotationY: 0,
+  info: {
+    title: `Dốc Lết - Ninh Hoà`,
+    artist: 'LeVietHaiQuan',
+    description: `Dốc Lết đẹp quá`,
+    year: `2025`,
+    link: 'https://www.facebook.com/imagoodsupport',
+  },
+},
+
   // Back Wall
   ...[
   {
@@ -27,11 +66,11 @@ export const paintingData = [
     position: { x: -15, y: 2, z: 19.5 },
     rotationY: Math.PI,
     info: {
-      title: `Van Gogh 5`,
-      artist: 'Vincent van Gogh',
-      description: `Artwork 5 by Vincent van Gogh is an exceptional piece showcasing his remarkable ability to capture emotion and atmosphere.`,
-      year: `Year 5`,
-      link: 'https://github.com/theringsofsaturn',
+      title: `QiQi x Biển Cam Ranh`,
+      artist: 'LeVietHaiQuan',
+      description: `Biển Cam Ranh đẹp quá`,
+      year: `2025`,
+      link: 'https://www.facebook.com/imagoodsupport',
     },
   },
   {
@@ -41,11 +80,11 @@ export const paintingData = [
     position: { x: -5, y: 2, z: 19.5 },
     rotationY: Math.PI,
     info: {
-      title: `Van Gogh 6`,
-      artist: 'Vincent van Gogh',
-      description: `Artwork 6 by Vincent van Gogh is an exceptional piece showcasing his remarkable ability to capture emotion and atmosphere.`,
-      year: `Year 6`,
-      link: 'https://github.com/theringsofsaturn',
+      title: `QiQi x Đồng Cỏ`,
+      artist: 'LeVietHaiQuan',
+      description:' Đồng Cỏ đẹp quá',
+      year: `2025`,
+      link: 'https://www.facebook.com/imagoodsupport',
     },
   },
   {
@@ -59,7 +98,7 @@ export const paintingData = [
     artist: 'CLB Hỗ trợ người nước ngoài học tập tại trường Đại học Mở TP.HCM', // <-- dùng lại key 'artist'
     year: `2025`,
     description: `Founded on Sept 20, 2024, the club supports international students at OU with extracurricular activities and mental well-being.`,
-    link: 'https://example.com/tranh7',
+    link: 'https://www.facebook.com/imagoodsupport',
     },
   },
   {
@@ -69,46 +108,130 @@ export const paintingData = [
     position: { x: 15, y: 2, z: 19.5 },
     rotationY: Math.PI,
     info: {
-      title: `Van Gogh 8`,
-      artist: 'Vincent van Gogh',
-      description: `Artwork 8 by Vincent van Gogh is an exceptional piece showcasing his remarkable ability to capture emotion and atmosphere.`,
-      year: `Year 8`,
-      link: 'https://github.com/theringsofsaturn',
+      title: `LeVietHaiQuan`,
+      artist: 'LeVietHaiQuan',
+      description: `LeVietHaiQuan chụp đẹp quá`,
+      year: `2025`,
+      link: 'https://www.facebook.com/imagoodsupport',
     },
   }
 ],
-  // Left Wall
-  ...Array.from({ length: 4 }, (_, i) => ({
-    imgSrc: `artworks/${i + 9}.jpg`,
+// Left Wall
+...[
+  {
+    imgSrc: `artworks/9.jpg`,
     width: 5,
     height: 3,
-    position: { x: -19.5, y: 2, z: -15 + 10 * i },
+    position: { x: -19.5, y: 2, z: -15 },
     rotationY: Math.PI / 2,
     info: {
-      title: `Van Gogh ${i + 9}`,
-      artist: 'Vincent van Gogh',
-      description: `With its striking use of color and brushwork, Artwork ${
-        i + 9
-      } is a testament to Van Gogh's artistic genius.`,
-      year: `Year ${i + 9}`,
-      link: 'https://github.com/theringsofsaturn',
+      title: `Tuyết Trinh x Đồng Cỏ`,
+      artist: 'LeVietHaiQuan',
+      description: `Hải Quân chụp đẹp quá`,
+      year: `2025`,
+      link: 'https://www.facebook.com/imagoodsupport',
     },
-  })),
-  // Right Wall
-  ...Array.from({ length: 4 }, (_, i) => ({
-    imgSrc: `artworks/${i + 13}.jpg`,
+  },
+  {
+    imgSrc: `artworks/10.jpg`,
     width: 5,
     height: 3,
-    position: { x: 19.5, y: 2, z: -15 + 10 * i },
+    position: { x: -19.5, y: 2, z: -5 },
+    rotationY: Math.PI / 2,
+    info: {
+      title: `LeVietHaiQuan`,
+      artist: 'LeVietHaiQuan',
+      description: `LeVietHaiQuan chụp đẹp quá`,
+      year: `2025`,
+      link: 'https://www.facebook.com/imagoodsupport',
+    },
+  },
+  {
+    imgSrc: `artworks/11.jpg`,
+    width: 5,
+    height: 3,
+    position: { x: -19.5, y: 2, z: 5 },
+    rotationY: Math.PI / 2,
+    info: {
+      title: `LeVietHaiQuan`,
+      artist: 'LeVietHaiQuan',
+      description: `LeVietHaiQuan chụp đẹp quá`,
+      year: `2025`,
+      link: 'https://www.facebook.com/imagoodsupport',  
+    },
+  },
+  {
+    imgSrc: `artworks/12.jpg`,
+    width: 5,
+    height: 3,
+    position: { x: -19.5, y: 2, z: 15 },
+    rotationY: Math.PI / 2,
+    info: {
+      title: `LeVietHaiQuan`,
+      artist: 'LeVietHaiQuan',
+      description: `LeVietHaiQuan chụp đẹp quá`,
+      year: `2025`,
+      link: 'https://www.facebook.com/imagoodsupport',
+    },
+  },
+  ],
+// Right Wall
+  ...[
+    {
+    imgSrc: `artworks/13.jpg`,
+    width: 5,
+    height: 3,
+    position: { x: 19.5, y: 2, z: -15 },
     rotationY: -Math.PI / 2,
     info: {
-      title: `Van Gogh ${i + 13}`,
-      artist: 'Vincent van Gogh',
-      description: `Artwork ${
-        i + 13
-      } is a captivating piece by Vincent van Gogh, reflecting his distinctive style and deep passion for art.`,
-      year: `Year ${i + 13}`,
-      link: 'https://github.com/theringsofsaturn',
+      title: `LeVietHaiQuan`,
+      artist: 'LeVietHaiQuan',
+      description: `LeVietHaiQuan chụp đẹp quá`,
+      year: `2025`,
+      link: 'https://www.facebook.com/imagoodsupport',
     },
-  })),
+  },
+  {
+    imgSrc: `artworks/14.jpg`,
+    width: 5,
+    height: 3,
+    position: { x: 19.5, y: 2, z: -5 },
+    rotationY: -Math.PI / 2,
+    info: {
+      title: `LeVietHaiQuan`,
+      artist: 'LeVietHaiQuan',
+      description: `LeVietHaiQuan chụp đẹp quá`,
+      year: `2025`,
+      link: 'https://www.facebook.com/imagoodsupport',
+    },
+  },
+  {
+    imgSrc: `artworks/15.JPG`,
+    width: 5,
+    height: 3,
+    position: { x: 19.5, y: 2, z: 5 },
+    rotationY: -Math.PI / 2,
+    info: {
+      title: `LeVietHaiQuan`,
+      artist: 'LeVietHaiQuan',
+      description: `LeVietHaiQuan chụp đẹp quá`,
+      year: `2025`,
+      link: 'https://www.facebook.com/imagoodsupport',
+    },
+  },
+  {
+    imgSrc: `artworks/16.JPG`,
+    width: 5,
+    height: 3,
+    position: { x: 19.5, y: 2, z: 15 },
+    rotationY: -Math.PI / 2,
+    info: {
+      title: `LeVietHaiQuan`,
+      artist: 'LeVietHaiQuan',
+      description: `LeVietHaiQuan chụp đẹp quá`,
+      year: `2025`,
+      link: 'https://www.facebook.com/imagoodsupport',
+    },
+  },
+  ],
 ];
--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
-  build: {
-    rollupOptions: {
-      input: 'main.js',
-    },
-  },
 });
-
-// This configuration sets the base path for the application to the root
-// and specifies the entry point for the build process as 'main.js'.
-// It uses Vite's defineConfig function to create a configuration object
-// that can be used to customize the build process.
-// The rollupOptions.input specifies the main entry file for the application,
-// which is 'main.js' in this case. This allows Vite to bundle the application
-// correctly, ensuring that all dependencies and modules are included in the final build.
-// This configuration is useful for single-page applications (SPAs) or   
